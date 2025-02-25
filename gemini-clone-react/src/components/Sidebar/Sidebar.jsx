688e599b131114cb4cd640bdc4aa1a58321b7eaf
ArrayArefin-07 <mobasherarefint2001@gmail.com>
2025-02-25 16:06:23 +0600

    stracture the component for main & sidebar component

---
+++ gemini-clone-react/src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './sidebar.css'
+import {assets} from '../../assets/assets'
+
+const Sidebar = () => {
+  return (
+    <div className='sidebar'>
+      <div className="top">
+        <img className='menu' src={assets.menu_icon} alt="" />
+        <div className="new-chat">
+          <img src="" alt="" />
+        </div>
+      </div>
+      <div className="bottom">
+
+      </div>
+    </div>
+  )
+}
+
+export default Sidebar
